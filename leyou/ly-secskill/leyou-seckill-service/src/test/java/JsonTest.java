6e8c54ca5ed9512cc80024ecb5fd0b9678fdb610
Your Name <you@example.com>
2020-03-17 20:49:03 +0800

    first

---
+++ leyou/ly-secskill/leyou-seckill-service/src/test/java/JsonTest.java
@@ -0,0 +1,20 @@
+import com.leyou.common.utils.JsonUtils;
+import com.leyou.seckill.vo.SeckillParameter;
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * @Author: 98050
+ * @Time: 2018-11-13 21:36
+ * @Feature:
+ */
+public class JsonTest {
+
+    @Test
+    public void json(){
+        SeckillParameter seckillParameter = new SeckillParameter();
+        seckillParameter.setEndTime(new Date());
+        System.out.println(JsonUtils.serialize(seckillParameter));
+    }
+}
